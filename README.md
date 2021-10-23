880de7f65a50f030635f73550ae4b5069399bc76
Julian Krämer <piscilus@gmail.com>
2021-10-23 13:04:39 +0200

    Initial commit of the templates

---
+++ README.md
@@ -0,0 +1,16 @@
+# File templates for C
+
+This repository provides file templates for source code written in C.
+
+The templates are intended for the modular programming of software. A typical 
+C module consists of a header file (\*.h) and a source file (\*.c). Templates 
+for both file types are provided.
+
+The files have a uniform structure for placing the individual parts of the 
+code.
+
+- compliant to the basic source character set
+- EOL character is 'LF'
+- limited to 80 characters per line (without EOL)
+- prepared for doxygen
+- MIT license
---
+++ source.h
@@ -0,0 +1,44 @@
+/**
+ * \file source.h
+ *
+ * \copyright (C) 2021 %%Author%%
+ *
+ * \brief %%BRIEF DESCRIPTION OF THE FILE/MODULE%%
+ */
+
+/*---- Include Guard ---------------------------------------------------------*/
+#ifndef SOURCE_H_
+#define SOURCE_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+/*---- Includes --------------------------------------------------------------*/
+
+
+/*---- Global macro definitions ----------------------------------------------*/
+
+
+/*---- Global type definitions -----------------------------------------------*/
+
+
+/*---- Exported constant declarations ----------------------------------------*/
+
+
+/*---- Exported data declarations --------------------------------------------*/
+
+
+/*---- Exported function declarations ----------------------------------------*/
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SOURCE_H_ */
+
+/*----------------------------------------------------------- END OF FILE ----*/
